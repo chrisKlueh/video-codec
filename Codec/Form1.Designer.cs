a95b9bef68d91e2f80621cbf59145fad806b3628
Samuel Dietz <samuel.dietz@informatik.hs-fulda.de>
2020-05-19 15:06:32 +0200

    necessary basics are implemented

--- Codec/Form1.Designer.cs
+++ Codec/Form1.Designer.cs
@@ -28,20 +28,130 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.inputSizeLabel = new System.Windows.Forms.Label();
+            this.outputSizeLabel = new System.Windows.Forms.Label();
+            this.timeBar = new System.Windows.Forms.TrackBar();
+            this.outputPictureBox = new System.Windows.Forms.PictureBox();
+            this.inputPictureBox = new System.Windows.Forms.PictureBox();
+            this.convertButton = new System.Windows.Forms.Button();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.progressLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.timeBar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.outputPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.inputPictureBox)).BeginInit();
             this.SuspendLayout();
+            // 
+            // inputSizeLabel
+            // 
+            this.inputSizeLabel.AutoSize = true;
+            this.inputSizeLabel.Location = new System.Drawing.Point(9, 255);
+            this.inputSizeLabel.Name = "inputSizeLabel";
+            this.inputSizeLabel.Size = new System.Drawing.Size(77, 13);
+            this.inputSizeLabel.TabIndex = 1;
+            this.inputSizeLabel.Text = "Input file size: -";
+            // 
+            // outputSizeLabel
+            // 
+            this.outputSizeLabel.AutoSize = true;
+            this.outputSizeLabel.Location = new System.Drawing.Point(335, 255);
+            this.outputSizeLabel.Name = "outputSizeLabel";
+            this.outputSizeLabel.Size = new System.Drawing.Size(85, 13);
+            this.outputSizeLabel.TabIndex = 3;
+            this.outputSizeLabel.Text = "Output file size: -";
+            // 
+            // timeBar
+            // 
+            this.timeBar.Location = new System.Drawing.Point(12, 281);
+            this.timeBar.Maximum = 299;
+            this.timeBar.Name = "timeBar";
+            this.timeBar.Size = new System.Drawing.Size(646, 45);
+            this.timeBar.TabIndex = 4;
+            this.timeBar.TickFrequency = 5;
+            this.timeBar.ValueChanged += new System.EventHandler(this.timeBar_ValueChanged);
+            // 
+            // outputPictureBox
+            // 
+            this.outputPictureBox.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.outputPictureBox.BackgroundImage = global::Codec.Properties.Resources.outputDefault;
+            this.outputPictureBox.Location = new System.Drawing.Point(338, 12);
+            this.outputPictureBox.Name = "outputPictureBox";
+            this.outputPictureBox.Size = new System.Drawing.Size(320, 240);
+            this.outputPictureBox.TabIndex = 2;
+            this.outputPictureBox.TabStop = false;
+            // 
+            // inputPictureBox
+            // 
+            this.inputPictureBox.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.inputPictureBox.BackgroundImage = global::Codec.Properties.Resources.inputDefault;
+            this.inputPictureBox.Location = new System.Drawing.Point(12, 12);
+            this.inputPictureBox.Name = "inputPictureBox";
+            this.inputPictureBox.Size = new System.Drawing.Size(320, 240);
+            this.inputPictureBox.TabIndex = 0;
+            this.inputPictureBox.TabStop = false;
+            this.inputPictureBox.Click += new System.EventHandler(this.inputPictureBox_Click);
+            // 
+            // convertButton
+            // 
+            this.convertButton.Location = new System.Drawing.Point(252, 319);
+            this.convertButton.Name = "convertButton";
+            this.convertButton.Size = new System.Drawing.Size(152, 33);
+            this.convertButton.TabIndex = 5;
+            this.convertButton.Text = "Convert video";
+            this.convertButton.UseVisualStyleBackColor = true;
+            this.convertButton.Click += new System.EventHandler(this.convertButton_Click);
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(12, 507);
+            this.progressBar.Maximum = 300;
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(646, 23);
+            this.progressBar.TabIndex = 6;
+            this.progressBar.Visible = false;
+            // 
+            // progressLabel
+            // 
+            this.progressLabel.AutoSize = true;
+            this.progressLabel.Location = new System.Drawing.Point(12, 488);
+            this.progressLabel.Name = "progressLabel";
+            this.progressLabel.Size = new System.Drawing.Size(53, 13);
+            this.progressLabel.TabIndex = 7;
+            this.progressLabel.Text = "working...";
+            this.progressLabel.Visible = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(670, 542);
+            this.Controls.Add(this.progressLabel);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.convertButton);
+            this.Controls.Add(this.timeBar);
+            this.Controls.Add(this.outputSizeLabel);
+            this.Controls.Add(this.outputPictureBox);
+            this.Controls.Add(this.inputSizeLabel);
+            this.Controls.Add(this.inputPictureBox);
             this.Name = "Form1";
             this.Text = "Codec Toolbox";
+            ((System.ComponentModel.ISupportInitialize)(this.timeBar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.outputPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.inputPictureBox)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox inputPictureBox;
+        private System.Windows.Forms.Label inputSizeLabel;
+        private System.Windows.Forms.PictureBox outputPictureBox;
+        private System.Windows.Forms.Label outputSizeLabel;
+        private System.Windows.Forms.TrackBar timeBar;
+        private System.Windows.Forms.Button convertButton;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Label progressLabel;
     }
 }
 
--- Codec/Form1.cs
+++ Codec/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,78 @@ namespace Codec
 {
     public partial class Form1 : Form
     {
+        string inputFileName = null;
+        Image[] inputImages = new Image[300];
+
+        string outputFile = null;
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        // If no input file is selected, clicking the input picture makes the user choose a file.
+        private void inputPictureBox_Click(object sender, EventArgs e)
+        {
+            if(inputFileName == null)
+            {
+                OpenFileDialog ofd = new OpenFileDialog();
+                ofd.InitialDirectory = Application.StartupPath;
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    inputFileName = ofd.FileName;
+                    inputSizeLabel.Text = "Input file size: " + BytesToString(new FileInfo(ofd.FileName).Length);
+                }
+                // Show conversion progress
+                progressLabel.Text = "Importing file...";
+                progressLabel.Visible = true;
+                progressBar.Value = 0;
+                progressBar.Visible = true;
+                // Convert input video to image array
+                var ffMpeg = new NReco.VideoConverter.FFMpegConverter();
+                for(int i = 0; i < 300; i++)
+                {
+                    using (MemoryStream stream = new MemoryStream())
+                    {
+                        // video has 60 fps
+                        ffMpeg.GetVideoThumbnail(inputFileName, stream, (i / 30f));
+                        inputImages[i] = Image.FromStream(stream);
+                    }
+                    progressBar.Value = i;
+                }
+                inputPictureBox.Image = inputImages[timeBar.Value];
+                progressLabel.Visible = false;
+                progressBar.Visible = false;
+            }
+        }
+
+        // show the chosen image(s)
+        private void timeBar_ValueChanged(object sender, EventArgs e)
+        {
+            inputPictureBox.Image = inputImages[timeBar.Value];
+        }
+
+        // Convert input using our codec
+        private void convertButton_Click(object sender, EventArgs e)
+        {
+            // TODO
+        }
+
+        #region Helper Methods
+
+        // easy way to display filesize how we are used to see it
+        // https://stackoverflow.com/questions/281640/how-do-i-get-a-human-readable-file-size-in-bytes-abbreviation-using-net
+        static String BytesToString(long byteCount)
+        {
+            string[] suf = { "B", "KB", "MB", "GB", "TB", "PB", "EB" };
+            if (byteCount == 0)
+                return "0" + suf[0];
+            long bytes = Math.Abs(byteCount);
+            int place = Convert.ToInt32(Math.Floor(Math.Log(bytes, 1024)));
+            double num = Math.Round(bytes / Math.Pow(1024, place), 1);
+            return (Math.Sign(byteCount) * num).ToString() + suf[place];
+        }
+
+        #endregion
     }
 }
