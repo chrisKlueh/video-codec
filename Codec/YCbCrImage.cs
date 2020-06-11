f0476f5fd82a92150e9c3f24a53b3a87162e619d
Samuel Dietz <samuel.dietz@informatik.hs-fulda.de>
2020-06-11 14:33:23 +0200

    fixed 4:2:0 chroma subsampling

--- Codec/YCbCrImage.cs
+++ Codec/YCbCrImage.cs
@@ -88,21 +88,51 @@ namespace Codec
             }
             else if (subsamplingMode == "4:2:0")
             {
+                double Cb;
+                double Cr;
                 if (x % 2 != 0 && y % 2 == 0)
                 {
-                    double Cb = (pixels[x - 1, y].getCb() + pixels[x + 1, y].getCb()) / 2;
-                    double Cr = (pixels[x - 1, y].getCr() + pixels[x + 1, y].getCr()) / 2;
+                    if (x == width - 1)
+                    {
+                        Cb = pixels[x - 1, y].getCb();
+                        Cr = pixels[x - 1, y].getCr();
+                    } else
+                    {
+                        Cb = (pixels[x - 1, y].getCb() + pixels[x + 1, y].getCb()) / 2;
+                        Cr = (pixels[x - 1, y].getCr() + pixels[x + 1, y].getCr()) / 2;
+                    }
                     return new YCbCrPixel(pixels[x, y].getY(), Cb, Cr);
                 } else if (x % 2 == 0 && y % 2 != 0)
                 {
-                    double Cb = (pixels[x, y - 1].getCb() + pixels[x, y + 1].getCb()) / 2;
-                    double Cr = (pixels[x, y - 1].getCr() + pixels[x, y + 1].getCr()) / 2;
+                    if (y == height - 1)
+                    {
+                        Cb = pixels[x, y - 1].getCb();
+                        Cr = pixels[x, y - 1].getCr();
+                    } else
+                    {
+                        Cb = (pixels[x, y - 1].getCb() + pixels[x, y + 1].getCb()) / 2;
+                        Cr = (pixels[x, y - 1].getCr() + pixels[x, y + 1].getCr()) / 2;
+                    }
                     return new YCbCrPixel(pixels[x, y].getY(), Cb, Cr);
                 }
-                else if (x % 2 == 0 && y % 2 == 0)
+                else if (x % 2 != 0 && y % 2 != 0)
                 {
-                    double Cb = (pixels[x - 1, y - 1].getCb() + pixels[x + 1, y + 1].getCb()) / 2;
-                    double Cr = (pixels[x - 1, y - 1].getCr() + pixels[x + 1, y + 1].getCr()) / 2;
+                    if (x == width - 1 && y == height - 1)
+                    {
+                        Cb = pixels[x - 1, y - 1].getCb();
+                        Cr = pixels[x - 1, y - 1].getCr();
+                    } else if (x == width - 1 && y != height - 1) {
+                        Cb = (pixels[x - 1, y - 1].getCb() + pixels[x - 1, y + 1].getCb()) / 2;
+                        Cr = (pixels[x - 1, y - 1].getCr() + pixels[x - 1, y + 1].getCr()) / 2;
+                    } else if (x != width - 1 && y == height - 1)
+                    {
+                        Cb = (pixels[x - 1, y - 1].getCb() + pixels[x + 1, y - 1].getCb()) / 2;
+                        Cr = (pixels[x - 1, y - 1].getCr() + pixels[x + 1, y - 1].getCr()) / 2;
+                    } else
+                    {
+                        Cb = (pixels[x - 1, y - 1].getCb() + pixels[x + 1, y + 1].getCb()) / 2;
+                        Cr = (pixels[x - 1, y - 1].getCr() + pixels[x + 1, y + 1].getCr()) / 2;
+                    }
                     return new YCbCrPixel(pixels[x, y].getY(), Cb, Cr);
                 }
             }
