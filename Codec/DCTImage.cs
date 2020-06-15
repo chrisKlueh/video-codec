fd15fb0ae652104ea20dd91103a684067ce2f381
Christian Klüh <60430668+chrisKlueh@users.noreply.github.com>
2020-06-15 12:11:25 +0200

    Change DctImage class

--- Codec/DCTImage.cs
+++ Codec/DCTImage.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Security.Cryptography;
 
-namespace Codec
+namespace ConsoleApp1
 {
     class DctImage
     {
@@ -9,73 +9,82 @@ namespace Codec
         //public int height { get; private set; }
         public YCbCrImage image;
         public static int m = 8, n = 8;
+        private int[,] quantizationMatrix = new int[8, 8] {
+            { 16, 11, 10, 16, 24, 40, 51, 61 },
+            { 12, 12, 14, 19, 26, 58, 60, 55 },
+            { 14, 13, 16, 24, 40, 57, 69, 56 },
+            { 14, 17, 22, 29, 51, 87, 80, 62 },
+            { 18, 22, 37, 56, 68, 109, 103, 77 },
+            { 24, 35, 55, 64, 81, 104, 113, 92 },
+            { 49, 64, 78, 87, 103, 121, 120, 101 },
+            { 72, 92, 95, 98, 112, 100, 103, 99 }};
 
         public DctImage(YCbCrImage image)
         {
-            //this.width = image.width.get;
+            //this.width = image.height.Get();
             //this.height = image.height.get;
             this.image = image;
         }
 
         //create a matrix containing only Y, Cb or Cr values
-        private double[,] FillValueMatrix(YCbCrImage image, String channelString)
+        public double[,] FillValueMatrix(YCbCrImage image, String channelString)
         {
-            double[,] valueMatrix = new double[image.width][image.height];
+            double[,] valueMatrix = new double[image.width, image.height];
             for (int height = 0; height < image.height; height++)
             {
                 for (int width = 0; width < image.width; width++)
                 {
+                    YCbCrPixel pixel = image.GetPixel(width, height);
+
+
                     switch (channelString)
                     {
                         case "Y":
-                            valueMatrix[width][height] = image.GetPixel(width, height).getY();
+                            valueMatrix[width, height] = pixel.getY();
                             break;
                         case "Cb":
-                            valueMatrix[width][height] = image.GetPixel(width, height).getCb();
+                            valueMatrix[width, height] = pixel.getCb();
                             break;
                         case "Cr":
-                            valueMatrix[width][height] = image.GetPixel(width, height).getCr();
+                            valueMatrix[width, height] = pixel.getCr();
                             break;
                         default:
                             break;
                     }
+
                 }
             }
             return valueMatrix;
         }
 
 
-        private double[,] PadValueMatrix(double[,] valueMatrix)
+        public double[,] PadValueMatrix(double[,] valueMatrix)
         {
             //calculate the additional columns and rows the paddedValueMatrix needs to have a multiple of 8 columns and rows
-            paddingWidth = 8 - (valueMatrix.GetLength(0) % 8);
-            paddingHeight = 8 - (valueMatrix.GetLength(1) % 8);
+            int paddingHeight = 8 - (valueMatrix.GetLength(0) % 8);
+            int paddingWidth = 8 - (valueMatrix.GetLength(1) % 8);
 
             //create the new matrix
-            double[,] paddedValueMatrix = new double[valueMatrix.GetLength(0) + paddingWidth][valueMatrix.GetLength(1) + paddingHeight];
+            double[,] paddedValueMatrix = new double[valueMatrix.GetLength(0) + paddingHeight, valueMatrix.GetLength(1) + paddingWidth];
 
-            for (int height = 0; height < paddedValueMatrix.height; height++)
+            for (int height = 0; height < paddedValueMatrix.GetLength(0); height++)
             {
-                for (int width = 0; width < paddedValueMatrix.width; width++)
+                for (int width = 0; width < paddedValueMatrix.GetLength(1); width++)
                 {
                     double value;
                     //fill paddedValueMatrix with all values of valueMatrix
-                    if (width < valueMatrix.GetLength(1) && height < valueMatrix.GetLength(1))
+                    if (width < valueMatrix.GetLength(1) && height < valueMatrix.GetLength(0))
                     {
-                        value = valueMatrix[width][height];
+                        value = valueMatrix[height, width];
                     }
-                    //fill the rest of paddedValueMatrix with 0.0 values
-                    else
-                    {
-                        value = 0.0;
-                    }
-                    paddedValueMatrix[width][height] = value;
+                    else { value = 0.0; }
+                    paddedValueMatrix[height, width] = value;
                 }
             }
             return paddedValueMatrix;
         }
 
-        
+
         public YCbCrImage PerformDct(YCbCrImage image)
         {
             YCbCrImage result = new YCbCrImage(image.width, image.height);
@@ -92,45 +101,49 @@ namespace Codec
             valueMatrixCb = DctSubArray(valueMatrixCb);
             valueMatrixCr = DctSubArray(valueMatrixCr);
 
-            for (int height = 0; height < image.height; height++)
+            for (int width = 0; width < image.width; width++)
+
             {
-                for (int width = 0; width < image.width; width++)
+                for (int height = 0; height < image.height; height++)
                 {
-                    result.getPixel(width, height).setY(valueMatrixY[width][height]);
-                    result.getPixel(width, height).setCb(valueMatrixCb[width][height]);
-                    result.getPixel(width, height).setCr(valueMatrixCr[width][height]);
+                    double Y = valueMatrixY[height, width];
+                    double Cb = valueMatrixCb[height, width];
+                    double Cr = valueMatrixCr[height, width];
+                    result.pixels[height, width] = new YCbCrPixel(Y, Cb, Cr);
                 }
             }
-            
+
             return result;
         }
 
         public double[,] DctSubArray(double[,] valueMatrix)
         {
-            double[,] resultMatrix = new double[valueMatrix.GetLength(0)][valueMatrix.GetLength(1)];
+            double[,] resultMatrix = new double[valueMatrix.GetLength(0), valueMatrix.GetLength(1)];
             //iterate the valueMatrix in 8x8 blocks
-            for (int height = 0; height < valueMatrix.GetLength(1) - 8; height += 8)
+            for (int width = 0; width <= valueMatrix.GetLength(1) - 8; width += 8)
             {
-                for (int width = 0; width < valueMatrix.GetLength(0) - 8; width += 8)
+                for (int height = 0; height <= valueMatrix.GetLength(0) - 8; height += 8)
                 {
                     //create a subArray for each block
-                    double[,] subArray = new double[8][8];
+                    double[,] subArray = new double[8, 8];
                     //fill the subArray with the values of the corresponding block in the valueMatrix
                     for (int subArrayY = 0; subArrayY < 8; subArrayY++)
                     {
                         for (int subArrayX = 0; subArrayX < 8; subArrayX++)
                         {
-                            subArray[subArrayX][subArrayY] = valueMatrix[width + subArrayX][height + subArrayY];
+                            subArray[subArrayY, subArrayX] = valueMatrix[height + subArrayY, width + subArrayX];
                         }
                     }
                     //perform Dct on subArray
                     subArray = Dct(subArray);
+                    //subArray = Quantization(subArray);
+
                     //fill corresponding block of resultMatrix with the values of subArray
                     for (int subArrayY = 0; subArrayY < 8; subArrayY++)
                     {
                         for (int subArrayX = 0; subArrayX < 8; subArrayX++)
                         {
-                            resultMatrix[width + subArrayX][height + subArrayY] = subArray[subArrayX][subArrayY];
+                            resultMatrix[height + subArrayY, width + subArrayX] = subArray[subArrayY, subArrayX];
                         }
                     }
                 }
@@ -139,87 +152,85 @@ namespace Codec
             return resultMatrix;
         }
 
-
+        /*
         //mock Dct algorithm for testing purposes
         public double[,] Dct(double[,] subArray)
         {
-            double[,] resultArray = new double[8][8];
-            for (int subArrayY = 0; subArrayY < 8; subArrayY++)
+            double[,] resultArray = new double[subArray.GetLength(0), subArray.GetLength(1)];
+            for (int subArrayY = 0; subArrayY < subArray.GetLength(0); subArrayY++)
             {
-                for (int subArrayX = 0; subArrayX < 8; subArrayX++)
+                for (int subArrayX = 0; subArrayX < subArray.GetLength(1); subArrayX++)
                 {
-                    resultArray[subArrayX][subArrayY] = resultArray[subArrayX][subArrayY] + 1;
+                    resultArray[subArrayX, subArrayY] = subArray[subArrayX, subArrayY] + 1;
                 }
             }
             return resultArray;
         }
+        */
 
-            /*
-            // https://www.geeksforgeeks.org/discrete-cosine-transform-algorithm-program/
-            // Function to find discrete cosine transform
-            public double[,] Dct(double[,] subArray)
+
+        // https://www.geeksforgeeks.org/discrete-cosine-transform-algorithm-program/
+        // Function to find discrete cosine transform
+        public double[,] Dct(double[,] subArray)
+        {
+            int i, j, k, l;
+            int width = subArray.GetLength(1);
+            int height = subArray.GetLength(0);
+
+            // dct will store the discrete cosine transform 
+            double[,] dct = new double[height, width];
+
+            double ci, cj, dct1, sum;
+
+            for (i = 0; i < height; i++)
             {
-                int i, j, k, l;
-
-                // dct will store the discrete cosine transform 
-                double[][] dct = new double[this.m][this.n];
-
-                double ci, cj, dct1, sum;
-
-                for (i = 0; i < this.m; i++)
+                for (j = 0; j < width; j++)
                 {
-                    for (j = 0; j < this.n; j++)
+                    // ci and cj depends on frequency as well as 
+                    // number of row and columns of specified matrix 
+                    if (i == 0)
+                        ci = 1 / Math.Sqrt(height);
+                    else
+                        ci = Math.Sqrt(2) / Math.Sqrt(height);
+
+                    if (j == 0)
+                        cj = 1 / Math.Sqrt(width);
+                    else
+                        cj = Math.Sqrt(2) / Math.Sqrt(width);
+
+                    // sum will temporarily store the sum of  
+                    // cosine signals 
+                    sum = 0;
+                    for (k = 0; k < height; k++)
                     {
-                        // ci and cj depends on frequency as well as 
-                        // number of row and columns of specified matrix 
-                        if (i == 0)
-                            ci = 1 / Math.sqrt(this.m);
-                        else
-                            ci = Math.sqrt(2) / Math.sqrt(this.m);
-
-                        if (j == 0)
-                            cj = 1 / Math.sqrt(this.n);
-                        else
-                            cj = Math.sqrt(2) / Math.sqrt(this.n);
-
-                        // sum will temporarily store the sum of  
-                        // cosine signals 
-                        sum = 0;
-                        for (k = 0; k < this.m; k++)
+                        for (l = 0; l < n; l++)
                         {
-                            for (l = 0; l < n; l++)
-                            {
-                                dct1 = subArray[k][l] * Math.cos((2 * k + 1) * i * Math.PI / (2 * this.m)) * Math.cos((2 * l + 1) * j * Math.PI / (2 * this.n));
-                                sum = sum + dct1;
-                            }
+                            dct1 = subArray[k, l] * Math.Cos((2 * k + 1) * i * Math.PI / (2 * height)) * Math.Cos((2 * l + 1) * j * Math.PI / (2 * width));
+                            sum = sum + dct1;
                         }
-                        dct[i][j] = ci * cj * sum;
                     }
+                    dct[i, j] = ci * cj * sum;
                 }
-
-                //Print result in console
-                //ONLY FOR TESTING
-                /*for (i = 0; i < this.m; i++)
-                {
-                    for (j = 0; j < this.n; j++)
-                    {
-                        Console.printf("%f\t", dct[i][j]);
-                    }
-                    Console.println();
-                }
-                */
-            /*
-                return dct;
-
             }
-            */
-
+            return dct;
         }
 
-    static void Main(string[] args)
-    {
-        // Display the number of command line arguments.
-        Console.WriteLine(args.Length);
+        public double[,] Quantization(double[,] subArray)
+        {
+            int width = subArray.GetLength(1);
+            int height = subArray.GetLength(0);
+
+
+            double[,] quantizedMatrix = new double[height, width];
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    quantizedMatrix[i, j] = Math.Round(subArray[i, j] / (double)this.quantizationMatrix[i, j]);
+                }
+            }
+            return quantizedMatrix;
+        }
     }
 }
 
