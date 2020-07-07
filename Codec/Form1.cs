f8e1ff580f74d7006c863f47164ca4d0f7f346d8
Samuel Dietz <samuel.dietz@informatik.hs-fulda.de>
2020-07-07 14:22:02 +0200

    added output file size

--- Codec/Form1.cs
+++ Codec/Form1.cs
@@ -225,6 +225,7 @@ namespace Codec
             IFormatter decodingFormatter = new BinaryFormatter();
             Stream decodingStream = new FileStream("akyio.bfv", FileMode.Open, FileAccess.Read, FileShare.Read);
             VideoFile inputVideo = (VideoFile)decodingFormatter.Deserialize(decodingStream);
+            outputSizeLabel.Text = "Output file size: " + BytesToString(decodingStream.Length);
             decodingStream.Close();
 
             //DCT & Quantization & Differential Decoding & Run Lenght Decoding & Huffman Decoding
